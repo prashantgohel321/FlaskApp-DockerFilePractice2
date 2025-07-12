9a5ba65e597cfff681efad5b494342ee159e316f
Prashant Gohel <smaterial123321@gmail.com>
2025-07-12 08:47:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Dosto, welcome to DevOps Zero To Hero (Junoon  Batch 9)'
-
-@app.route('/health')
-def health():
-    return 'Server is up and running'
+    return '#DevOpsWithUs\nHello DevOps Enthusiasts, I am Prashant Gohel!'
